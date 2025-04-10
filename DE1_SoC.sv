27248a571e959766a2c59fc14ec27dff4f66fd1f
kuhunain <155676663+kuhunain@users.noreply.github.com>
2025-04-09 20:49:18 -0700

    Add files via upload
    with parking lot

--- DE1_SoC.sv
+++ DE1_SoC.sv
@@ -7,12 +7,31 @@ module DE1_SoC (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, LEDR, V_GPIO);
 	output logic [9:0] LEDR;
 	inout  logic [35:0] V_GPIO;	// expansion header 0 (LabsLand board)
 	
-    // Turn off all 7-seg displays (active low: 1 = off, 0 = on)
-	assign HEX0 = 7'b1111111;
-	assign HEX1 = 7'b1111111;
-	assign HEX2 = 7'b1111111;
-	assign HEX3 = 7'b1111111;
-	assign HEX4 = 7'b1111111;
-	assign HEX5 = 7'b1111111;
+   // Internal signals for switches and LEDs
+    logic outer_switch, inner_switch;
+    logic outer_led, inner_led;
+
+    // === GPIO pin assignments ===
+    assign outer_switch = V_GPIO[24];     // Input from off-board switch
+    assign inner_switch = V_GPIO[25];     // Input from off-board switch
+
+    assign V_GPIO[34] = outer_led;        // Output to off-board LED
+    assign V_GPIO[35] = inner_led;        // Output to off-board LED
+
+    // Instantiate your parking lot system
+    parking_lot lot (
+        .clk(CLOCK_50),
+        .reset(1'b0),                     // You can connect this to a KEY button if desired
+        .outer_switch(outer_switch),
+        .inner_switch(inner_switch),
+        .outer_led(outer_led),
+        .inner_led(inner_led),
+        .HEX0(HEX0),
+        .HEX1(HEX1),
+        .HEX2(HEX2),
+        .HEX3(HEX3),
+        .HEX4(HEX4),
+        .HEX5(HEX5)
+    );
 
 endmodule  // DE1_SoC
